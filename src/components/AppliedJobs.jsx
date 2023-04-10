5f954d164d07a0c2d2f08bb8a97888cecd4e8c8f
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-10 15:06:31 +0600

    filter system added to applied job page

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJob from './AppliedJob';
 
 const AppliedJobs = () => {
-    const appliedJobs = useLoaderData()
-   
+    const getAppliedJobs = useLoaderData();
+    const [appliedJobs,setAppliedJobs] = useState(getAppliedJobs);
+
+    const handleRemoteJobsFilter = ()=>{
+        const remoteJobs = appliedJobs.filter(appliedJob => appliedJob.job_location !== 'Onsite'); 
+        setAppliedJobs(remoteJobs);
+    }
+    const handleOnsiteJobsFilter = ()=>{
+        const onSite = appliedJobs.filter(appliedJob => appliedJob.job_location !== 'Remote');
+        setAppliedJobs(onSite);
+    }
+
+
+
+
     return (
         <>
             <div className=' bg-[#D9D9D9] '>
                 <h1 className='pt-16 pb-28 text-center text-xl font-bold'>Applied Jobs</h1>
             </div>
             <div className='my-container text-right mt-24 mb-8'>
-                <button className='my-btn mr-3'>Remote</button>
-                <button className='my-btn'>Onsite</button>
+                <button onClick={handleRemoteJobsFilter} className='my-btn mr-3'>Remote</button>
+                <button onClick={handleOnsiteJobsFilter} className='my-btn'>Onsite</button>
             </div>
             <div >
                 {
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     console.log(error);
     return (
         <div className='flex flex-col h-[100vh] items-center justify-center'>
-            <h1 className='text-5xl font-semibold text-red-700'>{error.status || 404}</h1>
+            <h1 className='text-5xl font-semibold text-red-700'>{error.status || 404}!</h1>
             <h3 className='text-4xl ml-3'>{error.error.message}</h3>
         </div>
     );
